c1a0d4ffc256a5ab79cbfa004ec9e1f9d73db685
leandro <saltolks@gmail.com>
2024-11-22 16:53:09 -0300

    Meio aula mutantes

--- Aulas/src/main/java/org/example/Aula5/mutante/ex6/Mutante3.java
+++ Aulas/src/main/java/org/example/Aula5/mutante/ex6/Mutante3.java
@@ -8,6 +8,17 @@ public class Mutante3 {
     public String coverterParaRomano(int numero) {
         var ret = "";
         var i = 0;
+
+        /*
+        -------------------------------------------------------------------------
+        ESTE MUTANTE SERIA EQUIVALENTE, CASO TIVESSE ESTA VERIFICAÇÃO NO CÓDIGO.
+        -------------------------------------------------------------------------
+
+        if(numero < 0)
+        {
+            return "";
+        }
+        */
         while (numero != 0) {
             if (numero >= num[i]) {
                 ret += rom[i];
